7b8204d634bbd35ca26853e5153d28f2f7dd4f51
Siva-2511 <sivasubramaniyan.g2511@gmail.com>
2025-07-26 23:48:29 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleTheme() {
+    document.body.classList.toggle("dark-mode");
+    localStorage.setItem("theme", document.body.classList.contains("dark-mode") ? "dark" : "light");
+  }
+  
+  if (localStorage.getItem("theme") === "dark") {
+    document.body.classList.add("dark-mode");
+  }
